1752fc8d1ee283efdea2fad7d6c0b76cf3829e92
Sophia Heicksen <sheicksen@gmail.com>
2025-09-22 12:55:24 -0400

    Added functionality to main menu which loads the first scene when start button is pressed.

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,11 +1,4 @@
 extends Node
 
-var next_scene = "res://scenes/main_menu.tscn"
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var loading_screen = preload("res://scenes/loading_screen.tscn")
+var next_scene : String = "res://scenes/main_menu.tscn"
---
+++ scripts/loading_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ResourceLoader.load_threaded_request(Global.next_scene)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var progress = []
+	ResourceLoader.load_threaded_get_status(Global.next_scene, progress)
+	$ProgressBar.value = progress[0]*100
+	
+	if progress[0] == 1:
+		var loaded_scene = ResourceLoader.load_threaded_get(Global.next_scene)
+		get_tree().change_scene_to_packed(loaded_scene)
