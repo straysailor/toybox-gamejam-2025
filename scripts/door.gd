d856113ab69c0a6e3a8c7076384732238211f282
Sophia Heicksen <heicksen@udel.edu>
2025-09-25 14:34:03 -0400

    Created door class which controls where doors lead and how the indicator text is animated. Fixed localization issues with animation players/trees.

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,17 +1,47 @@
+class_name Door
 extends Area2D
 
-@onready var text = $InteractIndicator
+@export var text : String
+@export var side : String
+@export var color: Color
+@export var next_scene : String
+
+@onready var label_text = $InteractIndicator
 @onready var indicator = $InteractIndicator/IndicatorAnimation
-@onready var anim_tree = $InteractIndicator/AnimationTree
+@onready var left_anim_tree = $InteractIndicator/AnimTree_Left
+@onready var right_anim_tree = $InteractIndicator/AnimTree_Right
+
+var player_in_range = false
 
 func _on_ready() -> void:
-	text.label_settings.font_color = Color(255, 255, 255, 0.0)
+	label_text.label_settings.font_color = color
+	label_text.text = text
 
 func _on_area_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		anim_tree.set("parameters/fade_blend/blend_amount", 0.5) 
+		player_in_range = true
+		if side == "left":
+			left_anim_tree.active = true
+			left_anim_tree.set("parameters/fade_blend/blend_amount", 0.5) 
+		else:
+			right_anim_tree.active = true
+			right_anim_tree.set("parameters/fade_bounce_right/blend_amount", 0.5)
 
 func _on_area_exited(body: Node2D) -> void:
 	if body.name == "Player":
-		anim_tree.set("parameters/fade_blend/blend_amount", 1.0)
+		player_in_range = false
+		if side == "left":
+			left_anim_tree.set("parameters/fade_blend/blend_amount", 1.0) 
+			left_anim_tree.active = false
+		else:
+			right_anim_tree.set("parameters/fade_bounce_right/blend_amount", 1.0)
+			right_anim_tree.active = false
 		indicator.play("fade_out")
+
+		
+func _input(event:InputEvent) -> void:
+	if player_in_range and event.is_action_pressed("interact"):
+		Global.next_scene = next_scene;
+		get_tree().change_scene_to_packed(Global.loading_screen)
+		
+		
